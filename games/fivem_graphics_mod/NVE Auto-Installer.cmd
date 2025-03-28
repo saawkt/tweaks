12f5b5be25452eb06a0b225459db38494203406e
wasynxyxxk <202036399+wasynxyxxk@users.noreply.github.com>
2025-03-28 18:53:34 -0300

    2.1.0

--- games/fivem_graphics_mod/NVE Auto-Installer.cmd
+++ games/fivem_graphics_mod/NVE Auto-Installer.cmd
@@ -2,7 +2,36 @@
 title NVE Auto-Installer by sysnyxx
 setlocal enabledelayedexpansion
 
-:: mods
+:menu
+echo 1 - Aria2c (fast download)
+echo 2 - Powershell (+/- download)
+echo. 
+set /p input=": "
+if "%input%"=="1" (
+    goto aria2c
+) else if "%input%"=="2" (
+    goto powershell
+) else (
+    echo invalid choice. try again.
+    pause
+    goto menu
+)
+
+:aria2c
+echo downloading aria2c
+echo.
+curl -L "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0-win-64bit-build1.zip" -o "C:\Windows\Temp\aria2c.zip"
+powershell -command "Expand-Archive -Path 'C:\Windows\Temp\aria2c.zip' -DestinationPath 'C:\Windows\Temp\aria2c'
+cd /d C:\Windows\Temp\aria2c\aria2* >nul 2>&1
+cls
+echo downloading the mods, depending on the internet, can take between 5 and 20 minutes
+echo.
+aria2c -x 10 "https://drive.usercontent.google.com/download?id=1GHuDESNp-Xos92dYAe0EwrSiwa_aK1Pi&export=download&authuser=0&confirm=t&uuid=ffcc89ef-ed3a-42f6-847a-b0ce0dd07119&at=AEz70l7o09LkPhH1uW9BRGgHCM9a:1742787218530" -o "mods.zip"
+powershell -command "Expand-Archive -Path 'C:\Windows\Temp\mods.zip' -DestinationPath 'C:\Windows\Temp\mods'
+del /f /q C:\Windows\Temp\mods.zip >nul 2>&1
+goto main
+
+:powershell
 echo downloading the mods, depending on the internet, can take between 5 and 20 minutes
 echo. 
 curl -L "https://drive.usercontent.google.com/download?id=1GHuDESNp-Xos92dYAe0EwrSiwa_aK1Pi&export=download&authuser=0&confirm=t&uuid=ffcc89ef-ed3a-42f6-847a-b0ce0dd07119&at=AEz70l7o09LkPhH1uW9BRGgHCM9a:1742787218530" -o "C:\Windows\Temp\mods.zip"
@@ -11,7 +40,7 @@ echo wait...
 powershell -command "Expand-Archive -Path 'C:\Windows\Temp\mods.zip' -DestinationPath 'C:\Windows\Temp\mods'
 del /f /q C:\Windows\Temp\mods.zip >nul 2>&1
 
-:: main packages
+:main
 cd /d %localappdata%\FiveM\FiveM.app >nul 2>&1
 rmdir /s /q "mods" >nul 2>&1
 mkdir "mods" >nul 2>&1
